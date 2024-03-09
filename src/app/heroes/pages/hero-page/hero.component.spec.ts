4b944ab3e2a4828ae3d37867f5a532dfde95e96c
Francisco Matamoros <francisco.matamorosmoreno@plexus.es>
2024-03-10 00:33:16 +0100

    WIP Testing

--- src/app/heroes/pages/hero-page/hero.component.spec.ts
+++ src/app/heroes/pages/hero-page/hero.component.spec.ts
@@ -1,8 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { of } from 'rxjs';
 import { HeroesService } from '../../Services/heroes.service';
+import { Hero } from '../../interfaces/hero.interface';
 import { HeroComponent } from './hero.component';
 
 describe('HeroComponent', () => {
@@ -61,5 +67,34 @@ describe('HeroComponent', () => {
 
     expect(component.hero()).toEqual(mockHeroWithUpperCaseSuperhero);
     expect(component.heroForm.value).toEqual(mockHeroWithUpperCaseSuperhero);
+  });
+  it('should reset the form and set the image value to the hero image', () => {
+    const mockHero: Hero = {
+      id: '1',
+      img: 'hero-img-url',
+      superhero: 'Test Superhero',
+      publisher: 'Test Publisher',
+      alter_ego: 'Test Alter Ego',
+      first_appearance: 'Test First Appearance',
+      alt_img: 'test-alt-img-url',
+    };
+    const resetSpy = spyOn(component.heroForm, 'reset').and.callThrough();
+
+    component.hero.set(mockHero);
+    component.resetForm();
+
+    expect(resetSpy).toHaveBeenCalled();
+  });
+  it('should return true if field is required and has errors and has been touched', () => {
+    const mockForm: FormGroup = new FormBuilder().group({
+      superhero: ['', Validators.required],
+    });
+    mockForm.get('superhero')?.markAsTouched();
+
+    component.heroForm = mockForm;
+
+    const isValid = component.isValidFiled('superhero');
+
+    expect(isValid).toBeTruthy();
   });
 });
