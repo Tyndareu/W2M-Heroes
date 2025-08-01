419d3d03877434a37d8e4e460cf0f18e19b4e518
Tyndareus <flmatamoros@gmail.com>
2025-08-01 19:10:43 +0200

    fix: loading service patch

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { LoadingService } from './shared/services/loading/loading.service';
+import { LoadingService } from './shared/services/loading.service';
 
 describe('AppComponent', () => {
   let component: AppComponent;
